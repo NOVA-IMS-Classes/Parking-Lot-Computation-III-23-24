0821251d7229806e7c3e6cc7fb424b4407552a21
joaopfonseca <jpmrfonseca@gmail.com>
2023-11-13 12:39:48 +0000

    Format using black

--- parking_lot/__init__.py
+++ parking_lot/__init__.py
@@ -2,9 +2,4 @@ from .vehicles import Car, MotorBike, Moto4
 
 __version__ = "0.0.1"
 
-__all__ = [
-    "Car", 
-    "MotorBike", 
-    "Moto4",
-    "__version__"
-]
+__all__ = ["Car", "MotorBike", "Moto4", "__version__"]
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='parking_lot',
+    name="parking_lot",
     packages=find_packages(),
-    version='0.0.1',
-    description='Description goes here',
-    author='2023 Bsc in Information systems students',
-    license='MIT',
+    version="0.0.1",
+    description="Description goes here",
+    author="2023 Bsc in Information systems students",
+    license="MIT",
 )
