1b7f1f0086cdaf24a4e2405bbe43cf9f8a92114d
joaopfonseca <jpmrfonseca@gmail.com>
2023-11-27 12:31:26 +0000

    Add incomplete parking lot implementation

--- parking_lot/parkinglot.py
+++ parking_lot/parkinglot.py
@@ -1,2 +1,30 @@
+from parking_lot.vehicles import Car
+
+
 class ParkingLot:
-    pass
+    """
+    TODO: Add docstring
+    """
+
+    def __init__(self, total_slots, indoors, cleaning_services, vehicles=None):
+        self.total_slots = total_slots
+        self.indoors = indoors
+        self.cleaning_services = cleaning_services
+        self.vehicles = vehicles if vehicles is not None else []
+
+    def add_vehicle(self, vehicle):
+
+        if self.total_slots <= 0:
+            raise ValueError("There is no more space in the parking lot!")
+
+        if type(vehicle) == Car:
+            self.total_slots -= 2
+        else:
+            self.total_slots -= 1
+
+        self.vehicles.append(vehicle)
+
+        return self
+
+    def __len__(self):
+        return len(self.vehicles)
--- parking_lot/tests/test_parking_lot_class.py
+++ parking_lot/tests/test_parking_lot_class.py
@@ -3,6 +3,9 @@ from parking_lot import ParkingLot, Car, MotorBike, Moto4
 
 def test_basic_parking_lot_obj():
     pl = ParkingLot(total_slots=20, indoors=True, cleaning_services=True, vehicles=None)
+
+    assert pl.vehicles == []
+
     car = Car(
         "Honda Jazz",
         year=2003,
