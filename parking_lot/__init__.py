4ab528102de0b380c763ae283a3aaeb14c680b7a
joaopfonseca <jpmrfonseca@gmail.com>
2023-11-27 08:30:37 +0000

    Add Lecture 12 improvements

--- parking_lot/__init__.py
+++ parking_lot/__init__.py
@@ -1,5 +1,6 @@
 from .vehicles import Car, MotorBike, Moto4
+from .parkinglot import ParkingLot
 
 __version__ = "0.0.1"
 
-__all__ = ["Car", "MotorBike", "Moto4", "__version__"]
+__all__ = ["ParkingLot", "Car", "MotorBike", "Moto4", "__version__"]
